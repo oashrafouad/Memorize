9e4967fa51dd5c52771804bc7dc648e99044ec57
Omar Ashraf <oashrafouad@gmail.com>
2025-01-11 15:57:21 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Omar Ashraf on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
