d16f9fa8af1d2d6dc966de929fbea9467047fbfe
Omar Ashraf <oashrafouad@gmail.com>
2025-01-15 20:26:19 +0200

    initial laying out of the cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.orange)
         .padding()
     }
 }
 
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        if isFaceUp {
+            ZStack(content: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻")
+                    .font(.largeTitle)
+            })
+        } else {
+            RoundedRectangle(cornerRadius: 12)
+        }
+    }
+}
 #Preview {
     ContentView()
 }
