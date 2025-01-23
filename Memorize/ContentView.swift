eeef6841aea74aff85fe2393ceb6912587cabbc0
Omar Ashraf <oashrafouad@gmail.com>
2025-01-23 21:30:23 +0200

    laid out cards in a grid, added cards in scrollview

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -13,14 +13,17 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            cards
+            ScrollView {
+                cards
+            }
+            Spacer()
             cardCountAdjusters
         }
         .padding()
     }
     
     var cards: some View {
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
@@ -64,24 +67,19 @@ struct CardView: View {
     var body: some View {
         let base = RoundedRectangle(cornerRadius: 12)
         ZStack {
-            if isFaceUp {
-                // The first rectangle is for providing a white background in dark mode
-                base
-                    .fill(.white)
-                base
-                    .strokeBorder(lineWidth: 2)
-                Text(content)
-                    .font(.largeTitle)
+            // The first rectangle is for providing a white background in dark mode
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
-            else {
-                base.fill()
-            }
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }
+        .aspectRatio(2/3, contentMode: .fit)
         .onTapGesture {
             withAnimation {
                 isFaceUp.toggle()
             }
-            
         }
     }
 }
